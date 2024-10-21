52e1fd7b8d003aaee0f401441afb1f3742863f60
Jamie Rodriguez <65564846+fivetran-jamie@users.noreply.github.com>
2024-10-21 12:01:31 -0700

    added commented out conversion metrics to consistency tests for future use

--- integration_tests/tests/consistency/consistency_account_report.sql
+++ integration_tests/tests/consistency/consistency_account_report.sql
@@ -9,6 +9,9 @@ with prod as (
         sum(clicks) as clicks, 
         sum(impressions) as impressions,
         sum(spend) as spend
+        {# sum(conversions) as conversions,
+        sum(conversions_value) as conversions_value,
+        sum(all_conversions) as all_conversions #}
     from {{ target.schema }}_microsoft_ads_prod.microsoft_ads__account_report
     group by 1
 ),
@@ -19,6 +22,9 @@ dev as (
         sum(clicks) as clicks, 
         sum(impressions) as impressions,
         sum(spend) as spend
+        {# sum(conversions) as conversions,
+        sum(conversions_value) as conversions_value,
+        sum(all_conversions) as all_conversions #}
     from {{ target.schema }}_microsoft_ads_dev.microsoft_ads__account_report
     group by 1
 ),
@@ -32,6 +38,12 @@ final as (
         dev.impressions as dev_impressions,
         prod.spend as prod_spend,
         dev.spend as dev_spend
+        {# prod.conversions as prod_conversions,
+        dev.conversions as dev_conversions,
+        prod.conversions_value as prod_conversions_value,
+        dev.conversions_value as dev_conversions_value,
+        prod.all_conversions as prod_all_conversions,
+        dev.all_conversions as dev_all_conversions #}
     from prod
     full outer join dev 
         on dev.account_id = prod.account_id
@@ -43,3 +55,6 @@ where
     abs(prod_clicks - dev_clicks) >= .01
     or abs(prod_impressions - dev_impressions) >= .01
     or abs(prod_spend - dev_spend) >= .01
+    {# or abs(prod_conversions - dev_conversions) >= .01
+    or abs(conversions_value - dev_conversions_value) >= .01
+    or abs(all_conversions - dev_all_conversions) >= .01 #}
